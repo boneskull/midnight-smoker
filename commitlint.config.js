992362a0918a56ee593178e768b01f08745c3b00
Christopher Hiller <boneskull@boneskull.com>
2022-12-01 16:08:07 -0800

    chore: ignore line length in commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0],
+    'footer-max-line-length': [0],
+  },
+};
