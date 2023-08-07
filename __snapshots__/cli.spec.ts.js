e5460431d2db5f5a81533d440c3cb85bcaedc4f0
Christopher Hiller <boneskull@boneskull.com>
2023-08-06 23:34:11 -0700

    chore: fix snapshot test

--- __snapshots__/cli.spec.ts.js
+++ __snapshots__/cli.spec.ts.js
@@ -1,10 +1,13 @@
 exports['midnight-smoker CLI flag --help should show help text 1'] = {
-  "stdout": "smoker <script> [scripts..]\n\nRun tests against a package as it would be published\n\nPositionals:\n  script                                                                [string]\n  scripts                                                               [string]\n\nBehavior:\n  --add           Additional dependency to provide to smoke tests        [array]\n  --workspace     One or more npm workspaces to test                     [array]\n  --all           Test all workspaces                                  [boolean]\n  --include-root  Include the workspace root; must provide '--all'     [boolean]\n  --install-args  Extra arguments to pass to `npm install`               [array]\n  --dir           Working directory to use      [string] [default: new temp dir]\n  --force         Overwrite working directory if it exists             [boolean]\n  --clean         Truncate working directory; must provide '--force'   [boolean]\n  --npm           Path to `npm` executable     [string] [default: `npm` in PATH]\n  --verbose       Print output from npm                                [boolean]\n  --bail          When running scripts, halt on first error            [boolean]\n  --json          Output JSON only                                     [boolean]\n\nOptions:\n  --version  Show version number                                       [boolean]\n  --help     Show help                                                 [boolean]\n\nFor more info, see https://github.com/boneskull/midnight-smoker",
-  "stderr": "",
-  "exitCode": 0
-}
+  stdout:
+    "smoker <script> [scripts..]\n\nRun tests against a package as it would be published\n\nPositionals:\n  script                                                                [string]\n  scripts                                                               [string]\n\nBehavior:\n  --add           Additional dependency to provide to smoke tests        [array]\n  --workspace     One or more npm workspaces to test                     [array]\n  --all           Test all workspaces                                  [boolean]\n  --include-root  Include the workspace root; must provide '--all'     [boolean]\n  --install-args  Extra arguments to pass to `npm install`               [array]\n  --dir           Working directory to use      [string] [default: new temp dir]\n  --force         Overwrite working directory if it exists             [boolean]\n  --clean         Truncate working directory; must provide '--force'   [boolean]\n  --npm           Path to `npm` executable     [string] [default: `npm` in PATH]\n  --verbose       Print output from npm                                [boolean]\n  --bail          When running scripts, halt on first error            [boolean]\n  --json          Output JSON only                                     [boolean]\n\nOptions:\n  --version  Show version number                                       [boolean]\n  --help     Show help                                                 [boolean]\n\nFor more info, see https://github.com/boneskull/midnight-smoker",
+  stderr: '',
+  exitCode: 0,
+};
 
-exports['midnight-smoker CLI flag --json when the test fails should provide helpful result 1'] = `
+exports[
+  'midnight-smoker CLI flag --json when the test fails should provide helpful result 1'
+] = `
 {
   "results": [
     {
@@ -33,9 +36,11 @@ exports['midnight-smoker CLI flag --json when the test fails should provide help
   "failures": 1,
   "executed": 1
 }
-`
+`;
 
-exports['midnight-smoker CLI flag when the test passes should produce expected output 1'] = `
+exports[
+  'midnight-smoker CLI flag when the test passes should produce expected output 1'
+] = `
 💨 midnight-smoker v3.0.4
 - Packing current project...
 ✔ Packed 1 package
@@ -44,9 +49,11 @@ exports['midnight-smoker CLI flag when the test passes should produce expected o
 - Running script 0/1...
 ✔ Successfully ran 1 script
 ✔ Lovey-dovey! 💖
-`
+`;
 
-exports['midnight-smoker CLI flag when the test fails should produce expected output 1'] = `
+exports[
+  'midnight-smoker CLI flag when the test fails should produce expected output 1'
+] = `
 💨 midnight-smoker v3.0.4
 - Packing current project...
 ✔ Packed 1 package
@@ -57,15 +64,17 @@ exports['midnight-smoker CLI flag when the test fails should produce expected ou
 
 Error details for failed package fail:
 
-(runScript) Script "smoke" in package "fail" failed: Command failed with exit code 1: /home/boneskull/.nvm/versions/node/v20.5.0/bin/node /home/boneskull/.nvm/versions/node/v20.5.0/bin/npm run-script smoke
+(runScript) Script "smoke" in package "fail" failed: Command failed with exit code 1: <path/to/>/bin/node <path/to/>/bin/npm run-script smoke
 
 > fail@1.0.0 smoke
 > exit 1
 
 ✖ 🤮 Maurice!
-`
+`;
 
-exports['midnight-smoker CLI flag --json when the test passes should produce expected output 1'] = `
+exports[
+  'midnight-smoker CLI flag --json when the test passes should produce expected output 1'
+] = `
 {
   "results": [
     {
@@ -91,4 +100,4 @@ exports['midnight-smoker CLI flag --json when the test passes should produce exp
     "smoke:js"
   ]
 }
-`
+`;
