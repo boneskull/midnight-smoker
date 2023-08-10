5166298a6b8389199c05556bc6245168564b1f39
Christopher Hiller <boneskull@boneskull.com>
2023-08-10 00:37:02 -0700

    chore: add version datafiles to wallaby

--- .wallaby.js
+++ .wallaby.js
@@ -14,6 +14,7 @@ module.exports = () => {
       {pattern: './test/unit/mocks.ts', instrument: false},
       'package.json',
       '!./src/cli.ts',
+      {pattern: './data/*.json', instrument: false},
     ],
     testFramework: 'mocha',
     tests: ['./test/unit/**/*.spec.ts'],
