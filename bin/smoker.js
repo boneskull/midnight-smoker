d8b6966eda35f0cb4787ed26077fd1cac2ac5ddd
Christopher Hiller <boneskull@boneskull.com>
2023-08-15 15:44:05 -0700

    wip

--- bin/smoker.js
+++ bin/smoker.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {resolve} = require('node:path');
+const {runLava} = require('lavamoat');
+
+const ROOT = resolve(__dirname, '..');
+
+runLava({
+  projectRoot: ROOT,
+  policyPath: resolve(ROOT, 'lavamoat', 'node', 'policy.json'),
+  policyOverridePath: resolve(ROOT, 'lavamoat', 'node', 'policy-overrides.json'),
+  entryPath: require.resolve('../dist/cli.js'),
+});
+
 require('source-map-support').install();
 require('../dist/cli.js');
