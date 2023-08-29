de4d14c99a1cd4af26c30cc1dc2866348324f2d9
Christopher Hiller <boneskull@boneskull.com>
2023-08-29 15:06:24 -0700

    chore: regen schema when needed

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  '*.{js,ts}': ['eslint --fix', 'prettier --write'],
+  '*.md': ['markdownlint-cli2-fix', 'prettier --write'],
+  '*.ya?ml': ['prettier --write'],
+  /**
+   * This _should_ match if the options change, the rules change, the schema-generating script changes,
+   * or we've had dependency changes.
+   * @returns Command to update schema
+   */
+  '(./src/rules/**/*.ts|src/options.ts|./scripts/generate-schema.ts|./package.json|./package-lock.json)':
+    () => ['npm run update-schema'],
+};
--- scripts/generate-schema.ts
+++ scripts/generate-schema.ts
@@ -1,5 +1,10 @@
 #!/usr/bin/env ts-node
 
+/**
+ * Script to generate/update the JSON schema for `SmokerOptions`
+ * @module
+ */
+
 import path from 'node:path';
 import {writeFileSync} from 'node:fs';
 import {zodToJsonSchema} from 'zod-to-json-schema';
@@ -12,6 +17,13 @@ import {
 import {zRawSmokerOptions} from '../src/options';
 import {zCheckSeverity} from '../src/rules/severity';
 
+const DEST = path.join(
+  __dirname,
+  '..',
+  'schema',
+  'midnight-smoker.schema.json',
+);
+
 const jsonSchema = zodToJsonSchema(zRawSmokerOptions, {
   definitions: {
     defaultTrue: zTrue,
@@ -23,8 +35,6 @@ const jsonSchema = zodToJsonSchema(zRawSmokerOptions, {
   definitionPath: '$defs',
 });
 
-writeFileSync(
-  path.join(__dirname, '..', 'schema', 'midnight-smoker.schema.json'),
-  JSON.stringify(jsonSchema, null, 2),
-  'utf8',
-);
+writeFileSync(DEST, JSON.stringify(jsonSchema, null, 2), 'utf8');
+
+console.error(`Generated options schema at ${DEST}`);
