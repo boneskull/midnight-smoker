8e3193393bb669f4053dbc946920446c0d962aeb
Christopher Hiller <boneskull@boneskull.com>
2023-10-09 17:36:42 -0700

    chore(wallaby): update wallaby for monorepo

--- .wallaby.js
+++ .wallaby.js
@@ -5,19 +5,34 @@ module.exports = () => {
     env: {
       type: 'node',
       params: {
-        env: 'DEBUG=midnight-smoker',
+        env: 'DEBUG=midnight-smoker*',
       },
     },
     files: [
-      './src/**/*.ts',
-      {pattern: './bin/smoker.js', instrument: false},
-      {pattern: './test/unit/mocks.ts', instrument: false},
-      'package.json',
-      '!./src/cli.ts',
-      {pattern: './data/*.json', instrument: false},
+      './packages/*/src/**/*.ts',
+      {
+        pattern: './packages/midnight-smoker/bin/smoker.js',
+        instrument: false,
+      },
+      './packages/midnight-smoker/test/unit/mocks.ts',
+      './packages/midnight-smoker/test/unit/test-plugin.ts',
+      './packages/*/package.json',
+      '!./packages/midnight-smoker/src/cli.ts',
+      {
+        pattern: './packages/*/data/*.json',
+        instrument: false,
+      },
+      './packages/*/test/harness.ts',
+      {
+        pattern: './packages/*/test/**/fixture/**/*',
+        instrument: false,
+      },
     ],
     testFramework: 'mocha',
-    tests: ['./test/unit/**/*.spec.ts'],
+    tests: [
+      './packages/*/test/**/*.spec.ts',
+      '!./packages/*/test/e2e/**/*.spec.ts',
+    ],
     runMode: 'onsave',
     setup(wallaby) {
       process.env.WALLABY_PROJECT_DIR = wallaby.localProjectDir;
