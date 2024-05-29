45176ab2fda95f5f065bf534900575ea9bb0a274
Christopher Hiller <boneskull@boneskull.com>
2024-05-29 12:22:07 -0700

    chore(midnight-smoker): add missing AbortSignal.any

---
+++ packages/midnight-smoker/src/abortsignal.d.ts
@@ -0,0 +1,17 @@
+export {};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var AbortSignal: typeof globalThis extends {
+    onmessage: any;
+    AbortSignal: infer T;
+  }
+    ? T
+    : {
+        prototype: AbortSignal;
+        new (): AbortSignal;
+        abort(reason?: any): AbortSignal;
+        timeout(milliseconds: number): AbortSignal;
+        any(signals: AbortSignal[]): AbortSignal;
+      };
+}
